f4e4116e4e68405bfb94ee6edba5653ab4af4842
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-12 20:59:25 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,14 @@
 // your Bomb code here!
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state =
+  }
+
+  render() {
+    return(
+      <div>Hi</div>
+    )
+  }
+}
