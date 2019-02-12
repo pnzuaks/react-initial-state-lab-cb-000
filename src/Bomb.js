e93bf193065211d78a0658a3fc9da803ae226104
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-12 21:01:16 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -3,12 +3,12 @@ export default class Bomb extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {secondsLeft =}
+    this.state = {secondsLeft: this.props.initialCount}
   }
 
   render() {
     return(
-      <div>Hi</div>
+      <div>{this.state.secondsLeft ? `${this.state.}`}</div>
     )
   }
 }
